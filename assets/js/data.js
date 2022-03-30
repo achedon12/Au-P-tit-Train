073b55380fea01de652c4a5146159e5f376d8df7
Achedon12 <82544800+leoderoin@users.noreply.github.com>
2022-03-30 11:35:41 +0200

    update + test

---
+++ assets/js/data.js
@@ -0,0 +1,18 @@
+let data = [
+    {
+        pseudo: "administrateur",
+        password: "azerty123"
+    },
+    {
+        pseudo: "leo",
+        password: "monMDP"
+    },
+    {
+        pseudo: "thomas",
+        password: "monMDP"
+    },
+    {
+        pseudo: "leV",
+        password: "dictature"
+    }
+];
