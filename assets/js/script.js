07580180673988a84b11fcacc9f9f5e9c785d71c
Achedon12 <82544800+leoderoin@users.noreply.github.com>
2022-05-17 11:52:52 +0200

    add billets list to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 /* definitions des variables */
 let user = sessionStorage.getItem("user");
 let connexion = sessionStorage.getItem("connexion");
+let email = sessionStorage.getItem("email");
+let id = sessionStorage.getItem("id");
 let cgu2 = document.getElementById("cgu-2");
 let cgv2 = document.getElementById("cgv-2");
 let notifications2 = document.getElementById("notifications-2");
@@ -33,7 +35,6 @@ function doNotShow(){
     }
 }
 function connect(event){
-    let data = [];
     event.preventDefault();
     event.stopPropagation();
     fetch('http://gigondas:1111/sprietna/ihm/tp4/users')
@@ -57,6 +58,8 @@ function connect(event){
             for(element of user){
                 if(element.mail == mail){
                     sessionStorage.setItem("user",element.surname);
+                    sessionStorage.setItem("email",element.mail);
+                    sessionStorage.setItem("id",element.id);
                     sessionStorage.setItem("connexion",true);
                     console.log("Vous etes maintenant connecté sur le compte n°"+element.id);
                     window.location.replace("index.html");
@@ -117,6 +120,77 @@ function formToBillet(event){
             alert("Veuillez compléter toutes les données pour ajouter un billet à votre panier");
         }
 }
+function allBillets(){
+    fetch('http://gigondas:1111/sprietna/ihm/tp4/users/history/'+id)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }else{
+            throw response;
+        }
+    })
+    .then(billet => {
+        for(element of billet){
+            let surname;
+            let firstname;
+            let mail;
+            let data;
+            let departureTime;
+            let type;
+            let duration;
+            let cityFrom;
+            let cityFromID;
+            let cityFromName;
+            let cityTo;
+            let cityToID;
+            let cityToName;
+            console.log(element);
+            console.log("lol")
+            
+            surname = element.info.surname;
+            firstname = element.info.firstname;
+            mail = element.info.mail;
+             
+            date = element.schedule.date;
+            departureTime = element.schedule.departureTime;
+                    
+            type = element.schedule.type;
+                        
+            duration = element.schedule.travel.duration;
+                         
+            cityFrom = element.schedule.travel.from.city;
+            cityFromID = element.schedule.travel.from.id;
+            cityFromName = element.schedule.travel.from.name;
+                               
+            cityTo = element.schedule.travel.to.city;
+            cityToID = element.schedule.travel.to.id;
+            cityToName = element.schedule.travel.to.name;
+                               
+            let sectionNewBillet = document.createElement("section");
+            sectionNewBillet.classList.add("newBillet");
+            let h2_1 = document.createElement("h2");
+            h2_1.textContent = surname+" "+firstname;
+            sectionNewBillet.append(h2_1);
+            let h2_2 = document.createElement("h2");
+            h2_2.textContent = cityFromName+" - "+cityToName;
+            sectionNewBillet.append(h2_2);
+            let h2_3 = document.createElement("h2");
+            h2_3.textContent = departureTime;
+            sectionNewBillet.append(h2_3);
+            let button = document.createElement("button");
+            button.classList.add("annuler");
+            button.textContent = "Annuler le trajet";
+            sectionNewBillet.append(button);
+            let allBillets = document.querySelector("section.allBillets");
+            allBillets.insertBefore(sectionNewBillet,document.querySelector("section.newBillets"));    
+        }
+    })
+    .catch((error) => {
+        error.text.then(errorMessage => {
+            console.log('Request Failed :'+ errorMessage);
+        })
+    })
+}
 /* Events */
 cgu2.addEventListener("click",function(){
     doNotShow();
@@ -158,6 +232,16 @@ billets2.addEventListener("click",function(){
     doNotShow();
     tableau[7].style.display = "inherit";
     tableau[7].scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    if(connexion == null){
+        let noBillets = document.createElement("h2");
+        noBillets.textContent = "Veuillez vous connecter pour avoir accès à vos billets";
+        let allBillets = document.querySelector("#billets");
+        allBillets.removeChild(allBillets.lastChild);
+        allBillets.appendChild(noBillets);
+    }else{
+        allBillets();
+    }
+
 });
 basket2.addEventListener("click",function(){
     doNotShow();
@@ -231,7 +315,6 @@ if(connexion != null){
 }
 /* Easter Egg*/
 if(user == "le V" || user == "Le V"){
-    console.log("etz");
     let titleWithModif = document.createElement("h1");
     titleWithModif.textContent = "Au Grand V";
     titleWithModif.classList.add("underline");
